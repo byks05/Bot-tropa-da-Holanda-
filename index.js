da86e941767a94fc95e75e07dc5ef3094c291097
byks05 <kaiquesmc4@gmail.com>
2026-02-14 13:43:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -367,7 +367,7 @@ client.login(process.env.TOKEN);wayIntentBits,
   ButtonBuilder,
   ButtonStyle,
   StringSelectMenuBuilder
-} const { Client, GatewayIntentBits } = require("discord.js");
+ const { Client, GatewayIntentBits } = require("discord.js");
 require("dotenv").config();
 
 const client = new Client({
