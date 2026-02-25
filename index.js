f8d9e48dd8d5245b98a343eb6e9cbe9da8a37755
byks05 <kaiquesmc4@gmail.com>
2026-02-25 17:55:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1295,7 +1295,8 @@ app.get("/", (req, res) => {
   res.send("Bot online!");
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT;
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
